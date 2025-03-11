f6dae7036c99d495a39db67ca73481f2f544a658
VimForLaNie <new.giant098@gmail.com>
2025-03-11 21:14:04 +0700

    init svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { FileDropzone } from '@skeletonlabs/skeleton';
+
+	let files: FileList;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<FileDropzone name="input_images" bind:files={files}>
+		<svelte:fragment slot="message">Drag Your Images Here</svelte:fragment>
+		<svelte:fragment slot="meta">(or click to browse files)</svelte:fragment>
+	</FileDropzone>
+</section>
